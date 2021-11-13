d00e33d9c74b97e5a9363a68cad37d0f6e281a17
Eric <ngirabacu.eric@gmail.com>
2021-11-13 14:37:10 +0100

    updated hashing form & transaction generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
   title = 'oeblockchain';
   hashingTimer = 0;
   blockchain : Block[] = [];
-  genesisTransactions = ["x  q"];
+  genesisTransactions = ["x bought 20 tokens from q"];
   hash : any;
   genesisBlock : Block = new Block(0, this.genesisTransactions);
   block : any;
@@ -22,29 +22,51 @@ export class AppComponent {
 
   generateBlock(block? : any){
 
-    this.startCountDown();
-    
-    let lastBlock = this.blockchain[this.blockchain.length - 1];
-    let newChain : string[] = lastBlock.getTransactions();
-    newChain.push('test');
-    let newBlock = new Block(this.block, newChain);
-    this.blockchain.push(newBlock);
-    this.block = this.blockchain[this.blockchain.length - 1].getBlockHash();
-    
-  }
-
-  startCountDown() {
     this.hashingTimer = 10;
     let countDown = setInterval(() => {
       this.hashingTimer = this.hashingTimer - 1;
-      if(this.hashingTimer === 0) clearInterval(countDown)
-  }, 1000)
+      if(this.hashingTimer === 0) {
+        clearInterval(countDown)
+        let lastBlock = this.blockchain[this.blockchain.length - 1];
+        let newChain : string[] = lastBlock.getTransactions();
+        let newTransaction : string = this.generateTransaction();
+        this.hash ? newChain.push(this.hash) : newChain.push(newTransaction);
+        let newBlock = new Block(this.block, newChain);
+        this.blockchain.push(newBlock);
+        this.block = this.blockchain[this.blockchain.length - 1].getBlockHash();
+      }
+    }, 1000)
+    
+  }
+
+  randomIntFromInterval(min : number, max : number) { 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+  
+  generateTransaction() : string {
+
+    let a = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+    let b = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+    let actionList = [
+      "sold",
+      "bought"
+    ];
+    var action = actionList[Math.floor(Math.random()*actionList.length)];
+    let rndInt = this.randomIntFromInterval(1, 100);
+    let toOrFrom = '';
+    action == 'Sold' ? toOrFrom = ' to ' : toOrFrom = ' from ';
+    return a + ' ' + action + ' ' + rndInt + ' tokens' + toOrFrom + b;
+
+  }
+
+  startCountDown() {
+
   }
 
   onSubmit(form : any)  {
 
-    alert('submited');
-
+    this.generateBlock();
+    this.hash = '';
   }
 
   ngOnInit() {
