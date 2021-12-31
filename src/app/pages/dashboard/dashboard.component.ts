3960541835cc6dc8e1c307e21f3989b9ab39de9e
Eric <ngirabacu.eric@gmail.com>
2021-12-31 16:55:39 +0100

    Cleaned up dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { Block } from '../../block.model';
+import { AppState, Block } from '../../block.model';
 import * as BlockActions from '../../block.actions';
-
-interface AppState  {
-    blocks: any;
-}
 
 @Component({
   selector: 'app-dashboard',
@@ -14,77 +10,10 @@ interface AppState  {
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  title = 'oeblockchain';
-  hashingTimer = 0;
-  blockchain : Block[] = [];
-  genesisTransactions: string[] = [];
-  hash : any = '';
-  block : any;
-  previousBlock : string = "";
 
-  //from store
-  blocks$: Observable<any>;
-  blockchainstore: any;
-
-  // Store initialization
-  constructor(private store : Store<AppState>) {
-    this.blocks$ = this.store.select('blocks');
-
+  constructor() {
   }
 
-  // Generate the first block of the chain on init
   ngOnInit() {
-    this.blockchain = [new Block(0, [ this.generateTransaction() ])];
-    this.store.dispatch(new BlockActions.GetNewBlock(this.blockchain[0]))
-    this.block = this.blockchain[0].getBlockHash();
-    this.blocks$.subscribe(o => {
-      this.blockchainstore = o;
-    })
-  }
-
-  // Generate the next block 
-  onSubmit(form : any)  {
-    this.generateBlock(); 
-  }
-
-  generateBlock(block? : any){
-    this.hashingTimer = 3;
-    let countDown = setInterval(() => {
-      this.hashingTimer = this.hashingTimer - 1;
-      if(this.hashingTimer === 0) {
-        clearInterval(countDown)
-        let lastBlock = this.blockchain[0];
-        let newChain : string[] = lastBlock.getTransactions();
-        let newTransaction : string = this.generateTransaction();
-        this.hash ?
-          newChain = [this.hash, ...newChain] : 
-          newChain = [newTransaction.toString(), ...newChain];
-        let newBlock = new Block(this.block, newChain);
-        this.blockchain.unshift(newBlock);
-        this.block = this.blockchain[0].getBlockHash();
-        this.store.dispatch(new BlockActions.GetNewBlock(newBlock))
-        this.hash = '';
-      }
-    }, 1000)
-
-  }
-
-  randomIntFromInterval(min : number, max : number) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
-
-  // TODO temporary mock transactions
-  generateTransaction() : string {
-    let a = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-    let b = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-    let actionList = [
-      "sold",
-      "bought"
-    ];
-    var action = actionList[Math.floor(Math.random()*actionList.length)];
-    let rndInt = this.randomIntFromInterval(1, 100);
-    let toOrFrom = '';
-    action == 'Sold' ? toOrFrom = ' to ' : toOrFrom = ' from ';
-    return a + ' ' + action + ' ' + rndInt + ' tokens' + toOrFrom + b;
   }
 }
