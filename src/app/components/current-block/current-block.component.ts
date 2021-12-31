bbe93d8f09deca272f881198ab1a88d88e7a33a4
Eric <ngirabacu.eric@gmail.com>
2021-12-31 16:22:57 +0100

    Update current-block.component.ts

--- src/app/components/current-block/current-block.component.ts
+++ src/app/components/current-block/current-block.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Block } from '../../block';
+import { Block } from '../../block.model';
 
 @Component({
   selector: 'app-current-block',
